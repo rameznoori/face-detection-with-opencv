40caccc1e9360d700b8a3459e5de03472a1ebd2c
rameznoori <199920071+rameznoori@users.noreply.github.com>
2025-09-14 23:09:05 +0530

    loading the model and creating blob from image

--- face_detection.py
+++ face_detection.py
@@ -8,3 +8,10 @@ arg_p.add_argument("-p", "--prototxt", required=True, help="Path to Caffe 'deplo
 arg_p.add_argument("-m", "--model", required=True, help="Path to Caffe pre-trained model")
 arg_p.add_argument("-c", "--confidence", type=float, default=0.5, help="Minimum probability to filter weak detections")
 args = vars(arg_p.parse_args())
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (400, 400)), 1.0, (400, 400), (104.0, 177.0, 123.0))
